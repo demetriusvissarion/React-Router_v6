6890386be12ac5e054580d911581db4f4334c17e
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-02-12 19:30:47 +0000

    first commit

---
+++ src/app/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import About from "../components/About";
+import SignUp from "../components/SignUp";
+import Articles from "../components/Articles";
+import Article from "../components/Article";
+import Categories from "../components/Categories";
+import Category from "../components/Category";
+import Author from "../components/Author";
+import Profile from "../components/Profile";
+import EditProfileForm from "../components/EditProfileForm";
+import Root from "../components/Root";
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+
+import "./App.css";
+
+const router = (
+  <Route path="/" element={<Root />}>
+    <Route path="about" element={<About />} />
+  </Route>
+);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;

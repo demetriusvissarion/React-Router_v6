129ef55abe4f4a2c51e1538ed1993bb7cb33ad81
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-02-18 18:38:08 +0000

    finished author, profile & sign-up

--- src/app/App.js
+++ src/app/App.js
@@ -27,8 +27,12 @@ const router = createBrowserRouter(
       <Route path="articles" element={<Articles />} />
       <Route path="articles/:title" element={<Article />} />
       <Route path="authors/:name" element={<Author />} />
-      <Route path="categories" element={<Categories />} />
-      <Route path="profile" element={<Profile />} />
+      <Route path="categories" element={<Categories />}>
+        <Route path=":name" element={<Category />} />
+      </Route>
+      <Route path="profile" element={<Profile />}>
+        <Route path="edit" element={<EditProfileForm />} />
+      </Route>
     </Route>
   )
 );
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
+import { Link, Outlet, Navigate } from "react-router-dom";
+import {
+  selectCurrentUser,
+  selectIsLoggedIn,
+} from "../features/session/sessionSlice";
 
-export default function Profile () {
-  const currentUser = useSelector(selectCurrentUser)
+export default function Profile() {
+  const currentUser = useSelector(selectCurrentUser);
   const loggedIn = useSelector(selectIsLoggedIn);
-  
+
   // use loggedIn to return a Navigate
+  if (!loggedIn) {
+    return <Navigate to="/sign-up" />;
+  }
 
   return (
     <main>
       <h1>{currentUser.username}</h1>
       <Link to={`edit`}>Edit</Link>
-      {/* Tell React Router where to render child routes` */}
+      <Outlet />
     </main>
-  )
+  );
 }
